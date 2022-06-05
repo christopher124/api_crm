ff4ab7008f75562e3a160d0cd8f6e19be2db9a62
Christopher Sosa <urri_@hotmail.com>
2022-06-05 17:50:10 -0500

    Cambios en la api

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -9,7 +9,7 @@ export function Inicio() {
   useEffect(() => {
     const obtenerClientesAPI = async () => {
       try {
-        const url = "http://localhost:4000/clientes";
+        const url = import.meta.env.VITE_API_URL;
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
         setClientes(resultado);
@@ -29,7 +29,7 @@ export function Inicio() {
 
     if (confirmar) {
       try {
-        const url = `http://localhost:4000/clientes/${id}`;
+        const url = `${import.meta.env.VITE_API_URL}/${id}`;
         const respuesta = await fetch(url, {
           method: "DELETE",
         });
